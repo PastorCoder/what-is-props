beced423e941220854c85ab4ee82a229dae412c5
pastorcoder <zuma4good@gmail.com>
2021-10-01 02:49:50 +0100

    learning props in the real sense

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Greet from "./Components/Greet";
+import Welcome from "./Components/Welcome";
+import Hello from "./Components/Hello";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+  render(){
+    return (
+      <div className="App">
+        <Greet name="Bruce" heroName="Batman" >
+          <p>This is children props - this correnspnds to the innerHTML of the Greet component</p>
+          <h3>Will this 2nd prop show</h3>
+        </Greet>
+        <Greet name="Clark" heroName="Superman">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Dayana" heroName="Wonder Woman" /><hr />
+
+
+        <Welcome name="Bruce" heroName="Batman" />
+        <Welcome name="Clark" heroName="Superman" />
+        <Welcome name="Diana" heroName="Wonder Woman" />
+        {/* <Hello /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+/**Note */
+
+/**
+ * - Props (short for property(ies) is the optional input that your component can accept)
+ * - A prop is an object which contains the attribute and the values which have been passed 
+ * from the parent component
+ * - To specify props in a component, we specify them as attributes eg <Greet name="Bruce" />
+ * - After passing the name attribute, go to the component(in this case Greet.js) and add a parameter
+ *  to the functional componenet(you can call it props or any other name)
+ * - Use the parameter you passed into the function in the function body
+ * - if nothing is passed in the parent component(app.js), props.children(in Greet.js) renders nothing
+ * - in class based component, the props are available through this.props(which is reserved 
+ * keyword in class components)
+ * - One big rule is that props are immutable i.e the value cannot be changed
+ * - Tutorial Source - https://www.youtube.com/watch?v=m7OWXtbiXX8
+ */
